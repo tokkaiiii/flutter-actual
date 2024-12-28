e9fa503a2509c8d10418726d21d2e2b2c3bf390c
heosangbeom <luv_3_u@naver.com>
2024-12-28 14:32:36 +0900

    design(textfield): 텍스트 필드 생성
    - 텍스트 필드 생성

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
+import 'package:actual/common/component/custom_text_form_field.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    _App()
-  );
+  runApp(_App());
 }
 
 class _App extends StatelessWidget {
@@ -12,8 +11,26 @@ class _App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Container(),
+        backgroundColor: Colors.white,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CustomTextFormField(
+              hintText: '이메일을 입력해주세요',
+              onChanged: (String value){},
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            CustomTextFormField(
+              hintText: '비밀번호를 입력해주세요',
+              obscureText: true,
+              onChanged: (String value){},
+            ),
+          ],
+        ),
       ),
     );
   }
