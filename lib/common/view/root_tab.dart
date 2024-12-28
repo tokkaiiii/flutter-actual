87c5029c008393d1f0f03ab4f65dff0fcf790214
heosangbeom <luv_3_u@naver.com>
2024-12-29 01:12:33 +0900

    feat(ui): 탭바 추가
    - 탭바 추가

--- lib/common/view/root_tab.dart
+++ lib/common/view/root_tab.dart
@@ -1,17 +1,90 @@
+import 'package:actual/common/const/colors.dart';
 import 'package:actual/common/layout/default_layout.dart';
 import 'package:flutter/material.dart';
 
-class RootTab extends StatelessWidget {
+class RootTab extends StatefulWidget {
   const RootTab({super.key});
+
+  @override
+  State<RootTab> createState() => _RootTabState();
+}
+
+class _RootTabState extends State<RootTab> with SingleTickerProviderStateMixin {
+  late TabController controller;
+  int index = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TabController(length: 4, vsync: this);
+    controller.addListener(tabListener);
+
+  }
+
+  @override
+  void dispose() {
+    controller.removeListener(tabListener);
+
+    super.dispose();
+  }
+
+  void tabListener(){
+    setState(() {
+      index = controller.index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
-      child: Center(
-        child: Text(
-          'Root Tab',
-        ),
+      title: '코팩 딜리버리',
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: PRIMARY_COLOR,
+        unselectedItemColor: BODY_TEXT_COLOR,
+        selectedFontSize: 10,
+        unselectedFontSize: 10,
+        type: BottomNavigationBarType.shifting,
+        onTap: (int index) {
+          controller.animateTo(index);
+        },
+        currentIndex: index,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home_outlined,
+            ),
+            label: '홈',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.fastfood_outlined,
+            ),
+            label: '음식',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.receipt_long_outlined,
+            ),
+            label: '주문',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person_outlined,
+            ),
+            label: '프로필',
+          ),
+        ],
       ),
+        child: TabBarView(
+          physics: NeverScrollableScrollPhysics(),
+          controller: controller,
+          children: [
+            Center(child: Container(child: Text('홈'))),
+            Center(child: Container(child: Text('음식'))),
+            Center(child: Container(child: Text('주문'))),
+            Center(child: Container(child: Text('프로필'))),
+          ],
+        ),
     );
   }
 }
