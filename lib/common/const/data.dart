69af966f79bccbc82551f455a54433a5e85c42e2
heosangbeom <luv_3_u@naver.com>
2024-12-30 14:29:32 +0900

    feat(restaurant): restaurant 관련 ui 제작
    - restaurant 관련 ui 제작

--- lib/common/const/data.dart
+++ lib/common/const/data.dart
@@ -1,6 +1,14 @@
+import 'dart:io';
+
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 const ACCESS_TOKEN_KEY = 'ACCESS_TOKEN';
 const REFRESH_TOKEN_KEY = 'REFRESH_TOKEN';
+
+// localhost
+final emulatorId = '10.0.2.2:3000';
+final simulatorId = '127.0.0.1:3000';
+
+final ip = Platform.isIOS ? simulatorId : emulatorId;
 
 final storage = FlutterSecureStorage();
--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -1,8 +1,26 @@
+import 'package:actual/common/const/data.dart';
 import 'package:actual/restaurant/component/restaurant_card.dart';
+import 'package:actual/restaurant/model/restaurant_model.dart';
+import 'package:actual/restaurant/view/restaurant_detail.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class RestaurantScreen extends StatelessWidget {
   const RestaurantScreen({super.key});
+
+  Future<List> paginateRestaurant() async {
+    final dio = Dio();
+    final accessToken = await storage.read(key: ACCESS_TOKEN_KEY);
+
+    final resp = await dio.get(
+      'http://$ip/restaurant',
+      options: Options(
+        headers: {'authorization': 'Bearer $accessToken'},
+      ),
+    );
+
+    return resp.data['data'];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,17 +28,42 @@ class RestaurantScreen extends StatelessWidget {
       child: Center(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: RestaurantCard(
-            image: Image.asset(
-              'asset/img/food/ddeok_bok_gi.jpg',
-              fit: BoxFit.cover,
-            ),
-            name: '불타는 떡볶이',
-            tags: ['떡볶이', '치즈', '매운맛'],
-            ratingCount: 100,
-            deliveryTime: 15,
-            deliveryFee: 2000,
-            rating: 4.52,
+          child: FutureBuilder<List>(
+            future: paginateRestaurant(),
+            builder: (context, AsyncSnapshot<List> snapshot) {
+              if (!snapshot.hasData) {
+                return Container();
+              }
+
+              return ListView.separated(
+                itemBuilder: (_, index) {
+                  final item = snapshot.data![index];
+                  final pItem = RestaurantModel.fromJson(
+                    json: item,
+                  );
+                  // parsed
+
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (_) => RestaurantDetail(),
+                        ),
+                      );
+                    },
+                    child: RestaurantCard.fromModel(
+                      model: pItem,
+                    ),
+                  );
+                },
+                separatorBuilder: (_, index) {
+                  return SizedBox(
+                    height: 16.0,
+                  );
+                },
+                itemCount: snapshot.data!.length,
+              );
+            },
           ),
         ),
       ),
